24f96ebfcff1ebd58878b78894c6c721251cea7f
comedial <comedial@student.21-school.ru>
2023-10-31 13:44:48 +0300

    00 done

--- src/ex00/Chat/SocketClient/src/main/java/edu/school21/sockets/client/Client.java
+++ src/ex00/Chat/SocketClient/src/main/java/edu/school21/sockets/client/Client.java
@@ -6,15 +6,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", Integer.parseInt(args[0].substring(args[0].indexOf('=') + 1)));
+public class Client {
+    public static void start(int port) throws IOException {
+        Socket socket = new Socket("localhost", port);
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedReader consoleIn =  new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(inFromServer.readLine());
-
         out.println(consoleIn.readLine());
         String serverMessage = "";
         while (!(serverMessage = inFromServer.readLine()).equals("Successful!")) {
