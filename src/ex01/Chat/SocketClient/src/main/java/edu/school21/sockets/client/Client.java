3eb895e0994a239515d218b1482426bdcd4fd9fe
comedial <comedial@student.21-school.ru>
2023-11-07 23:46:45 +0300

    01 client done

--- src/ex01/Chat/SocketClient/src/main/java/edu/school21/sockets/client/Client.java
+++ src/ex01/Chat/SocketClient/src/main/java/edu/school21/sockets/client/Client.java
@@ -7,24 +7,72 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
+
+
+
     public static void start(int port) throws IOException {
-        Socket socket = new Socket("localhost", port);
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(inFromServer.readLine());
-        out.println(consoleIn.readLine());
-        while (socket.isConnected()) {
-            String serverMessage = inFromServer.readLine();
-            System.out.println(serverMessage);
-            if(serverMessage.equals("Incorrect username or password")) {
-                break;
+        try (Socket socket = new Socket("localhost", port);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+
+            String name = "";
+
+            while (true) {
+                String fromServer = in.readLine();
+                System.out.println(fromServer);
+                if (fromServer.equals("Start messaging")) break;
+                String consoleInput = consoleIn.readLine();
+                if (consoleInput.equals("Exit") || fromServer.equals("Incorrect username or password")) System.exit(0);
+                out.println(consoleInput);
+                if (fromServer.equals("Enter username:")) {
+                    name = consoleInput;
+                }
             }
-            out.println(consoleIn.readLine());
+
+            new InputThread(name, in).start();
+
+            String userInput;
+            while ((userInput = consoleIn.readLine()) != null) {
+                if (userInput.equals("Exit")) {
+                    out.println(userInput);
+                    in.readLine();
+                    System.exit(0);
+                }
+                out.println(name + ": " + userInput);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        inFromServer.close();
-        out.close();
-        socket.close();
 
     }
+
+    private static class InputThread extends Thread {
+        private String name;
+        private BufferedReader in;
+
+        public InputThread(String name, BufferedReader in) {
+            this.name = name;
+            this.in = in;
+        }
+
+        public void run() {
+            String message;
+            try {
+                while ((message = in.readLine()) != null) {
+                    String[] splitedMessage = message.split(": ");
+                    String userName = splitedMessage[0];
+                    if (!userName.equals(name)) {
+                        System.out.println(message);
+                    }
+
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
